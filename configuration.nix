c8266d727a589f0b8062f9880fb8609e0176ee57
HannahPadd <hannah@lindrob.nl>
2026-03-01 13:53:08 +0100

    update docker setup

--- configuration.nix
+++ configuration.nix
@@ -112,8 +112,16 @@
   virtualisation.docker.enable = true;
 
   virtualisation.docker.rootless = {
-  enable = true;
-  setSocketVariable = true;
+    enable = true;
+    setSocketVariable = true;
+  };
+  security.wrappers = {
+    docker-rootlesskit = {
+      owner = "root";
+      group = "root";
+      capabilities = "cap_net_bind_service+ep";
+      source = "${pkgs.rootlesskit}/bin/rootlesskit";
+    };
   };
 
   # Allow unfree packages
